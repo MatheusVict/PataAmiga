20488f875a764303b1136ac8f8e0a8a482ce4072
MatheusVict <matheusvictorhenrique@gmail.com>
2023-05-04 15:13:39 -0300

    splash screen, app icon and app label

--- app/src/main/java/com/example/pawfriend/MainActivity.kt
+++ app/src/main/java/com/example/pawfriend/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.pawfriend
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#FFFFFFFF")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
