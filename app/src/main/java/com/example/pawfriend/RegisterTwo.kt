531e90310e87fc5329e5488c7d55f2d4a61e6f14
MatheusVict <matheusvictorhenrique@gmail.com>
2023-05-14 21:37:09 -0300

    register two refact total

--- app/src/main/java/com/example/pawfriend/RegisterTwo.kt
+++ app/src/main/java/com/example/pawfriend/RegisterTwo.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.util.Patterns
 import android.widget.Toast
 import com.example.pawfriend.databinding.ActivityRegisterOneBinding
 import com.example.pawfriend.databinding.ActivityRegisterTwoBinding
@@ -23,35 +24,80 @@ class RegisterTwo : AppCompatActivity() {
         val password: String? = intent.getStringExtra("passwordUser")
         val name: String? = intent.getStringExtra("nameUser")
         val birth: String? = intent.getStringExtra("birthUser")
+        phoneFocusListener()
+        whatsappFocusListener()
+        locationFocusListener()
 
-        var erros: Int = 0
+        binding.registerTwoButton.setOnClickListener { submitForm() }
+    }
 
-        // TODO: add number regexx
+    private fun submitForm() {
+        binding.phoneNumber.error = validPhone()
+        binding.whatsappInput.error = validWhatsapp()
+        binding.locationInput.error = validLocation()
 
+        val validPhone = binding.phoneNumber.error == null
+        val validWhatsapp = binding.whatsappInput.error == null
+        val validLocation = binding.locationInput.error == null
 
-        binding.registerTwoButton.setOnClickListener {
-            if (binding.phoneNumber.text.toString().isNotEmpty() && binding.phoneNumber.text.toString().length == 11) {
-                binding.phoneNumber.error = null
-                erros --
+        val toastMessage: String = getString(R.string.toast_error_inputs)
 
-            } else {
-                binding.phoneNumber.error = getString(R.string.inputEmptyError)
-                erros ++
+        if (validLocation && validPhone && validWhatsapp) {
+            // TODO: create user in database and validate
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        } else Toast.makeText(applicationContext, toastMessage, Toast.LENGTH_LONG).show()
+    }
+
+    private fun phoneFocusListener() {
+        binding.phoneNumber.setOnFocusChangeListener {_, focused ->
+            if (!focused) {
+                binding.phoneNumber.error = validPhone()
             }
-            if (binding.whatsappInput.text.toString().isNotEmpty() && binding.whatsappInput.text.toString().length == 11) {
-                binding.whatsappInput.error = null
-                erros --
-
-            } else {
-                binding.whatsappInput.error = getString(R.string.inputEmptyError)
-                erros ++
-            }
-            if(erros > 0) {
-                Toast.makeText(applicationContext, "preencja", Toast.LENGTH_LONG).show()
-            } // TODO: API connection 
         }
+    }
 
+    private fun validPhone(): String? {
+        val isDone = binding.phoneNumber.isDone
+        if (!isDone) {
+            return getString(R.string.inputEmptyError)
+        }
+        return null
+    }
 
+    private fun whatsappFocusListener() {
+        binding.whatsappInput.setOnFocusChangeListener {_, focused ->
+            if (!focused) {
+                binding.whatsappInput.error = validWhatsapp()
+            }
+        }
+    }
+
+    private fun validWhatsapp(): String? {
+        val isDone = binding.whatsappInput.isDone
+        if (!isDone) {
+            return getString(R.string.inputEmptyError)
+        }
+        return null
+    }
+
+    private fun locationFocusListener() {
+        binding.locationInput.setOnFocusChangeListener {_, focused ->
+            if (!focused) {
+                binding.locationInput.error = validLocation()
+            }
+        }
+    }
+
+    private fun validLocation(): String? {
+        val locationText = binding.locationInput.text.toString()
+        if (locationText.isEmpty()) {
+            return getString(R.string.inputEmptyError)
+        }
+        if (locationText.length < 5) {
+            return getString(R.string.register_two_location_length)
+        }
+        return null
     }
 
 }
